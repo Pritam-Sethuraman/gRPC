a5a43f55ef7a54e1162319ce82b18a44ff5d2410
Pritam Sethuraman <pritamsethuraman@gmail.com>
2023-10-18 11:58:09 -0400

    Updated Client

--- client.py
+++ client.py
@@ -1,41 +1,162 @@
+import inventory_pb2_grpc
+import inventory_pb2
 import grpc
-from inventory_pb2_grpc import InventoryServiceStub
-from inventory_pb2 import InventoryRequest, InventorySearchRequest, InventoryRangeRequest, DistributionRequest, UpdateRequest
+import pandas as pd
+
+def search_in_excel(file_path):
+    df = pd.read_excel(file_path)
+    result = df.to_dict(orient='records')
+    return result
 
 def run():
-    channel = grpc.insecure_channel('localhost:50051')
-    client = InventoryServiceStub(channel)
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = inventory_pb2_grpc.InventoryServiceStub(channel)
+        excel_file_path = r'InventoryData.xlsx'
 
-    # Search by ID
-    request = InventoryRequest(id='IN0002')
-    response = client.searchByID(request)
-    print("Search by ID:")
-    print(response)
+        inventory_data = search_in_excel(excel_file_path)
 
-    # Search by Key and Key Value
-    search_request = InventorySearchRequest(key_name='Inventory ID', key_value='IN0003')
-    response = client.search(search_request)
-    print("Search by Key and Key Value:")
-    print(response)
+        if not inventory_data:
+            print("No Inventory data found in the Excel file.")
+            return
 
-    # Search by Key and Range
-    range_request = InventoryRangeRequest(key_name='Quantity in Reorder', key_value_start=50, key_value_end=150)
-    response = client.searchRange(range_request)
-    print("Search by Key and Range:")
-    for record in response:
-        print(record)
+        while True:
+            print("1. Search by ID")
+            print("2. Search by Key Value pair")
+            print("3. Search within a range")
+            print("4. Calculate percentile")
+            print("5. Update a value")
+            print("6. Exit")
+            choice = input("Which gRPC operation would you like to perform (1/2/3/4/5/6): ")
 
-    # Get Distribution
-    dist_request = DistributionRequest(key_name='Quantity in Stock', percentile=50)
-    response = client.getDistribution(dist_request)
-    print("Get Distribution (50th percentile):")
-    print(response)
+            if choice == '1':
+                inventory_id = input("Enter the Inventory ID to search: ")
 
-    # Update Record
-    update_request = UpdateRequest(key_name='Inventory ID', key_value='IN0004', val_name='Quantity in Stock', val_val_new=60)
-    response = client.update(update_request)
-    print("Update Record:")
-    print(response)
+                # Search for the Inventory ID in the local data
+                found_inventory = next((item for item in inventory_data if item['Inventory_ID'] == inventory_id), None)
 
-if __name__ == '__main__':
+                # ...
+
+                if found_inventory:
+                    print("\nFound in local data:")
+                    print("Local data details:")
+                    for key, value in found_inventory.items():
+                        print(f"{key}: {value}")
+
+                    # Search for the Inventory ID on the gRPC server
+                    try:
+                        response = stub.searchByID(inventory_pb2.InventoryRequest(id=inventory_id))
+                        if response.Inventory_ID:
+                            print("\nResult from gRPC server:")
+                            print("gRPC server response details:")
+                            print(f"Inventory_ID: {response.Inventory_ID}")
+                            print(f"Name: {response.Name}")
+                            print(f"Description: {response.Description}")
+                            print(f"Price: {response.Price}")
+                            print(f"Quantity_in_Stock: {response.Quantity_in_Stock}")
+                            print(f"Quantity_in_Reorder: {response.Quantity_in_Reorder}")
+                            print(f"Discontinued: {response.Discontinued}")
+                        else:
+                            print(f"Error: {response}")
+                    except grpc.RpcError as e:
+                        # Handle NOT_FOUND error
+                        if e.code() == grpc.StatusCode.NOT_FOUND:
+                            print("Inventory ID", inventory_id, "not found on the gRPC server.")
+                        else:
+                            print(f"Error: {e}")
+                else:
+                    print("Inventory ID", inventory_id, "not found in the local data.")
+
+
+            elif choice == '2':
+                key_name = input("Enter the key name: ")
+                key_value = input("Enter the key value: ")
+                try:
+                    response = stub.search(inventory_pb2.InventorySearchRequest(key_name=key_name, key_value=key_value))
+                    if response.Inventory_ID:
+                        print("\nResult from gRPC server:")
+                        print("gRPC server response details:")
+                        print(f"Inventory_ID: {response.Inventory_ID}")
+                        print(f"Name: {response.Name}")
+                        print(f"Description: {response.Description}")
+                        print(f"Price: {response.Price}")
+                        print(f"Quantity_in_Stock: {response.Quantity_in_Stock}")
+                        print(f"Quantity_in_Reorder: {response.Quantity_in_Reorder}")
+                        print(f"Discontinued: {response.Discontinued}")
+                    else:
+                        print(f"Error: {response}")
+                except grpc.RpcError as e:
+                    # Handle NOT_FOUND error
+                    if e.code() == grpc.StatusCode.NOT_FOUND:
+                        print(f"Key '{key_name}' with value '{key_value}' not found on the gRPC server.")
+                    else:
+                        print(f"Error: {e}")
+
+            elif choice == '3':
+                key_name = input("Enter the key name: ")
+                key_value_start = input("Enter the start value: ")
+                key_value_end = input("Enter the end value: ")
+
+                try:
+                    response_iterator = stub.searchRange(
+                        inventory_pb2.InventoryRangeRequest(
+                            key_name=key_name,
+                            key_value_start=key_value_start,
+                            key_value_end=key_value_end
+                        )
+                    )
+
+                    print("\nResult from gRPC server:")
+                    print("gRPC server response details:")
+                    for response in response_iterator:
+                        print(f"Inventory_ID: {response.Inventory_ID}")
+                        print(f"Name: {response.Name}")
+                        print(f"Description: {response.Description}")
+                        print(f"Price: {response.Price}")
+                        print(f"Quantity_in_Stock: {response.Quantity_in_Stock}")
+                        print(f"Quantity_in_Reorder: {response.Quantity_in_Reorder}")
+                        print(f"Discontinued: {response.Discontinued}")
+
+                except grpc.RpcError as e:
+                    print(f"Error: {e}")
+
+            elif choice == '4':
+                key_name = input("Enter the key name: ")
+                percentile = float(input("Enter the percentile: "))
+                try:
+                    response = stub.getDistribution(inventory_pb2.DistributionRequest(key_name=key_name, percentile=percentile))
+                    print("\nResult from gRPC server:")
+                    print("gRPC server response details:")
+                    print(f"Percentile Value: {response.value}")
+                except grpc.RpcError as e:
+                    print(f"Error: {e}")
+
+            elif choice == '5':
+                key_name = input("Enter the key name: ")
+                key_value = input("Enter the key value: ")
+                val_name = input("Enter the attribute name to update: ")
+                val_val_new = input("Enter the new value: ")
+
+                try:
+                    response = stub.update(inventory_pb2.UpdateRequest(
+                        key_name=key_name,
+                        key_value=key_value,
+                        val_name=val_name,
+                        val_val_new=val_val_new
+                    ))
+
+                    if response.success:
+                        print("\nUpdate successful.")
+                    else:
+                        print("Update failed. Record not found on the gRPC server.")
+                except grpc.RpcError as e:
+                    print(f"Error: {e}")
+
+            elif choice =='6':
+                print("Exiting Program")
+                break  # Exit the loop
+
+            else:
+                print("Invalid choice. Please enter 1, 2, 3, 4, 5 or 6.")
+
+if __name__ == "__main__":
     run()
--- server.py
+++ server.py
@@ -1,51 +1,160 @@
-import grpc
-import pandas as pd  # Import Pandas library for reading Excel files
 from concurrent import futures
-from inventory_pb2_grpc import InventoryServiceServicer, add_InventoryServiceServicer_to_server
-from inventory_pb2 import InventoryRequest, InventoryRecord, InventorySearchRequest, InventoryRangeRequest, DistributionRequest, UpdateRequest, UpdateResponse
+import grpc
+import pandas as pd
+import inventory_pb2
+import inventory_pb2_grpc
+import numpy as np
+import math
 
-# Load inventory data from Excel file
-inventory_data = pd.read_excel('inventoryData.xlsx')
+class InventoryServiceServicer(inventory_pb2_grpc.InventoryServiceServicer):
+    def __init__(self, excel_file_path):
+        self.excel_file_path = excel_file_path
+        self.inventory_data = self.load_inventory_data()
 
-class InventoryServiceServicer(InventoryServiceServicer):
+    def load_inventory_data(self):
+        df = pd.read_excel(self.excel_file_path)
+        return df.to_dict(orient='records')
+
     def searchByID(self, request, context):
         inventory_id = request.id
-        record = inventory_data[inventory_data['Inventory ID'] == inventory_id]
-        if not record.empty:
-            record_dict = record.to_dict(orient='records')[0]
-            return InventoryRecord(**record_dict)
-        return InventoryRecord()  # Return an empty record if not found
+
+        # Search for the Inventory ID in the local data
+        found_inventory = next((item for item in self.inventory_data if item['Inventory_ID'] == inventory_id), None)
+
+        if found_inventory:
+            # Convert the found inventory data to an InventoryRecord message
+            discontinued_value = found_inventory.get('Discontinued')
+
+            # Check if the Discontinued value is None or an empty string
+            if discontinued_value is None or str(discontinued_value).strip() == "":
+                discontinued_bool = False  # Set to False for empty values
+            else:
+                # Check if the Discontinued value is a number (float)
+                if isinstance(discontinued_value, (int, float)):
+                    discontinued_bool = bool(discontinued_value)
+                else:
+                    # Check if the Discontinued value is "yes"
+                    if str(discontinued_value).strip().lower() == "yes":
+                        discontinued_bool = True
+                    else:
+                        try:
+                            # Check if the Discontinued value is NaN
+                            discontinued_bool = math.isnan(float(discontinued_value))
+                        except (TypeError, ValueError):
+                            # If it's not a number or cannot be converted to NaN, set to False
+                            discontinued_bool = False
+
+            return inventory_pb2.InventoryRecord(
+                Inventory_ID=str(found_inventory['Inventory_ID']),
+                Name=found_inventory['Name'],
+                Description=found_inventory['Description'],
+                Price=float(found_inventory['Price']),
+                Quantity_in_Stock=int(found_inventory['Quantity_in_Stock']),
+                Quantity_in_Reorder=int(found_inventory['Quantity_in_Reorder']),
+                Discontinued=discontinued_bool
+            )
+        else:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"Inventory ID '{inventory_id}' not found.")
+            return inventory_pb2.InventoryRecord()  # Return an empty InventoryRecord
+
+    def searchFullRowByID(self, request, context):
+        inventory_id = request.id
+
+        # Search for the Inventory ID in the local data
+        found_inventory = next((item for item in self.inventory_data if item['Inventory_ID'] == inventory_id), None)
+
+        if found_inventory:
+            # Return the entire row of data for the found inventory as a dictionary
+            return {
+                'Inventory_ID': str(found_inventory['Inventory_ID']),
+                'Name': found_inventory['Name'],
+                'Description': found_inventory['Description'],
+                'Price': float(found_inventory['Price']),
+                'Quantity_in_Stock': int(found_inventory['Quantity_in_Stock']),
+                'Quantity_in_Reorder': int(found_inventory['Quantity_in_Reorder']),
+                'Discontinued': bool(found_inventory['Discontinued'])
+            }
+        else:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"Inventory ID '{inventory_id}' not found.")
+            return {}  # Return an empty dictionary
 
     def search(self, request, context):
         key_name = request.key_name
-        key_value = request.key_value
-        record = inventory_data[inventory_data[key_name] == key_value]
-        if not record.empty:
-            record_dict = record.to_dict(orient='records')[0]
-            return InventoryRecord(**record_dict)
-        return InventoryRecord()  # Return an empty record if not found
+        key_value = str(request.key_value).lstrip('$')  # Convert to string and remove dollar sign
+
+        # Search for the key and value in the local data
+        found_inventory = next(
+            (item for item in self.inventory_data if str(item.get(key_name)).lstrip('$') == key_value), None)
+
+        if found_inventory:
+            # Convert the found inventory data to an InventoryRecord message
+            return inventory_pb2.InventoryRecord(
+                Inventory_ID=str(found_inventory['Inventory_ID']),
+                Name=found_inventory['Name'],
+                Description=found_inventory['Description'],
+                Price=float(found_inventory['Price']),
+                Quantity_in_Stock=int(found_inventory['Quantity_in_Stock']),
+                Quantity_in_Reorder=int(found_inventory['Quantity_in_Reorder']),
+                Discontinued=bool(found_inventory['Discontinued']) if 'Discontinued' in found_inventory else False
+            )
+        else:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"Key '{key_name}' with value '{key_value}' not found.")
+            return inventory_pb2.InventoryRecord()  # Return an empty InventoryRecord
 
     def searchRange(self, request, context):
         key_name = request.key_name
         key_value_start = request.key_value_start
         key_value_end = request.key_value_end
-        records = inventory_data[
-            (inventory_data[key_name] >= key_value_start) & (inventory_data[key_name] <= key_value_end)
+
+        # Convert key values to float for numeric comparisons
+        key_value_start = float(key_value_start)
+        key_value_end = float(key_value_end)
+
+        # Search for the key within the specified range in the local data
+        matching_inventory = [
+            item for item in self.inventory_data
+            if key_name in item and
+               isinstance(item[key_name], (int, float)) and
+               key_value_start <= float(item[key_name]) <= key_value_end
         ]
-        records_dict = records.to_dict(orient='records')
-        return [InventoryRecord(**record_dict) for record_dict in records_dict]
+
+        for found_inventory in matching_inventory:
+            # Convert the found inventory data to an InventoryRecord message
+            yield inventory_pb2.InventoryRecord(
+                Inventory_ID=str(found_inventory['Inventory_ID']),
+                Name=found_inventory['Name'],
+                Description=found_inventory['Description'],
+                Price=float(found_inventory['Price']),
+                Quantity_in_Stock=int(found_inventory['Quantity_in_Stock']),
+                Quantity_in_Reorder=int(found_inventory['Quantity_in_Reorder']),
+                Discontinued=bool(found_inventory['Discontinued']) if 'Discontinued' in found_inventory else False
+            )
 
     def getDistribution(self, request, context):
         key_name = request.key_name
-        values = inventory_data[key_name].tolist()
-        values.sort()
-        total_records = len(values)
-        if total_records == 0:
-            return None  # Key not found
+        percentile = request.percentile
 
-        # Calculate the percentile value
-        index = int((request.percentile / 100) * total_records)
-        return float(values[index])
+        # Extract the values for the specified key from the local data
+        values = [item.get(key_name, 0) for item in self.inventory_data]
+
+        if not values:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"Key '{key_name}' not found in the inventory data.")
+            return inventory_pb2.DistributionResponse(value=None)
+
+        # Calculate the percentile value with numpy.percentile
+        result = self.calculate_percentile(values, percentile)
+
+        return inventory_pb2.DistributionResponse(value=result)
+
+    def calculate_percentile(self, values, percentile):
+        # Use numpy.percentile for accurate percentile calculation
+        result = np.percentile(values, percentile, interpolation='linear')
+
+        return result
 
     def update(self, request, context):
         key_name = request.key_name
@@ -53,21 +162,37 @@ class InventoryServiceServicer(InventoryServiceServicer):
         val_name = request.val_name
         val_val_new = request.val_val_new
 
-        record = inventory_data[inventory_data[key_name] == key_value]
+        # Find the record with the given key name and key value
+        record_index = next((index for index, item in enumerate(self.inventory_data)
+                             if item.get(key_name) == key_value), None)
 
-        if not record.empty:
-            inventory_data.at[record.index, val_name] = val_val_new
-            return UpdateResponse(success=True)
-        return UpdateResponse(success=False)  # Return false if record not found
+        if record_index is not None:
+            # Update the specified attribute with the new value
+            self.inventory_data[record_index][val_name] = val_val_new
+
+            # Save the updated data back to the Excel file
+            self.save_inventory_data()
+
+            return inventory_pb2.UpdateResponse(success=True)
+        else:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"Record with '{key_name}'='{key_value}' not found.")
+            return inventory_pb2.UpdateResponse(success=False)
+
+    def save_inventory_data(self):
+        # Save the updated data back to the Excel file
+        updated_df = pd.DataFrame(self.inventory_data)
+        updated_df.to_excel(self.excel_file_path, index=False)
+
 
 def serve():
+    excel_file_path = r'InventoryData.xlsx'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    my_servicer = InventoryServiceServicer()
-    add_InventoryServiceServicer_to_server(my_servicer, server)
-    server.add_insecure_port('[::]:50051')
+    inventory_service = InventoryServiceServicer(excel_file_path)
+    inventory_pb2_grpc.add_InventoryServiceServicer_to_server(inventory_service, server)
+    server.add_insecure_port("localhost:50051")
     server.start()
-    print("Server started. Listening on port 50051.")
     server.wait_for_termination()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     serve()
